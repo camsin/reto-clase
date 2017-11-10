0f592689ceb62751b17a8b22f4a0de12024e18bf
Kenia Delgado <a291450@uach.mx>
2017-11-09 19:40:02 -0700

    Subiendo modelo user

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+
+const schema = mongoose.Schema;
+
+const userSchema = schema({
+  name: String,
+  age: String,
+  sex: String,
+});
+
+module.exports = mongoose.model('User', userSchema);
