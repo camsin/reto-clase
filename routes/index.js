ee6c3492d185c74f6132fbb2a43123ef27639ea5
Kenia Delgado <a291450@uach.mx>
2017-11-09 20:10:06 -0700

    lol

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const indexController = require('../controllers/index/index');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', indexController.index);
 
 module.exports = router;
